31e8a4a3db9feccf85c42dd65d392b445bcb8f81
KSKAkhil <kskakhil@gmail.com>
2019-06-25 00:01:27 +0530

    b2gs part-2

--- src/lab/diff/SRIP/Codes/b2gs.py
+++ src/lab/diff/SRIP/Codes/b2gs.py
@@ -6,26 +6,31 @@ Created on Mon Jun 24 16:35:41 2019
 """
 import random
 import numpy as np
-def Distance_Transform(x):
-    print(x);
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 #edge dect.
 a= np.zeros((16, 16));
     #a= np.array([random.sample(range(16),c) for x in range(r)]);
     #print(a);
 for i in range(0,16):
     for j in range(0,16):
-          a[i,j] = random.randrange(0, 2);
-   
+         a[i,j] = random.randrange(1, 2);
+         
+def Distance_Transform(x):
+    r = len(x)
+    c = len(list(zip(*x)))
+    #return np.shape(x)
+    print(x);
+    print(r);
+    print(c)
+    for i in range(0,16):
+        for j in range(0,16):
+            v1 = min(x[i-1, j-1],x[i, j-1],x[i-1, j]);
+            v2 = min(x[i-1, j+1],x[i+1, j],x[i+1, j+1]);
+            print(v1);
+            print(v2);
+            print("ok")
+            #x[i,j]= v+1;
+    #return     
+
+         
+         
 Distance_Transform(a);
